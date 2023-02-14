1c6caf616460e29f1a24da2a8131ea531cb9eb74
Joseph Lin <lin1223@purdue.edu>
2023-02-13 20:56:26 -0500

    added histogram

--- algorithm/Edge_Detection/sobel.py
+++ algorithm/Edge_Detection/sobel.py
@@ -1,8 +1,10 @@
 # Joseph Lin 2/5/2023
 # Use cv2 to run sobel operator
+# added histogram of the sobel edge detection image
 
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 # Load the image
 img = cv2.imread("iraqi10000ar_180.jpg", cv2.IMREAD_GRAYSCALE)
@@ -17,4 +19,11 @@ result = cv2.normalize(result, None, 0, 255, cv2.NORM_MINMAX)
 result = result.astype(np.uint8)
 
 # Save the result
-cv2.imwrite("result.jpg", result)
+plt.subplot(121), plt.imshow(result, cmap='gray')
+plt.title("Soble Edge Detection"), plt.xticks([]), plt.yticks([])
+plt.show()
+
+# Plot the histogram
+plt.hist(img.ravel(), bins=256, range=(0, 256), fc='k', ec='k')
+plt.title("image histogram")
+plt.show()
