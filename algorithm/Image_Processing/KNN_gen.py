aeab81a22fad7c4628a7a96302bd4293199f7ac2
Katherine <katherinesandys@gmail.com>
2023-03-05 20:10:05 -0500

    KNN update
    KNN update with constants, this allows the code to function correctly

--- algorithm/Image_Processing/KNN_gen.py
+++ algorithm/Image_Processing/KNN_gen.py
@@ -1,16 +1,15 @@
-from numpy import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = random.uniform(low = -100, high = 100, size = [50])
-x = random.uniform(low = -100, high = 100, size = [50])
+y = np.random.uniform(low = -100, high = 100, size = [50])
+x = np.random.uniform(low = -100, high = 100, size = [50])
 
-for i in range(25): #round
+for i in range(50): #round
     x[i] = round(x[i], 1)
     y[i] = round(y[i], 1)
 
-print(x) #the array
-print(y)
+print(x.tolist())  # the array
+print(y.tolist())
 
 classed = []
 
