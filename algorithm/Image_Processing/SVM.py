801d8b4b36c45946c130899b972c52c18f1e03f3
Katherine <katherinesandys@gmail.com>
2023-03-07 09:02:21 -0500

    Create SVM.py
    Setup the file, not finished yet.
    Will be finished at a later date

---
+++ algorithm/Image_Processing/SVM.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+# Katherine Sandys (3/5/2023)
+# this function does SVM
+#
+# code returns 	0 for Thai
+            # 1 for Colombia
+            # 2 for UAE
+            # 3 for Hong Kong
+            # 4 Neither
+
+def svm():
+
+
+
+    return 0
